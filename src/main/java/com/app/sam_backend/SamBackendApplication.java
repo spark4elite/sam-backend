f65f8a141ba1768d4a6fa49ef746a824d8a25fe7
arjunm385 <arjunm385@gmail.com>
2024-12-09 12:42:04 +0530

    initial setup

---
+++ src/main/java/com/app/sam_backend/SamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.sam_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/sam_backend/SamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sam_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
